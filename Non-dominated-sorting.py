9a81c5e7ae477eb96d1b289894c6c69b956135c7
BhargavaKrishnaSreepathi <sreepathi@arch.ethz.ch>
2017-06-03 15:12:07 +0800

    new

---
+++ Non-dominated-sorting.py
@@ -0,0 +1,112 @@
+"""
+=======================================
+Selection of Pareto Optimal individuals
+=======================================
+
+"""
+
+from __future__ import division
+from deap import tools
+import pandas as pd
+import numpy as np
+
+__author__ = "Sreepathi Bhargava Krishna"
+
+
+def identify_pareto_observations(book):
+
+    pareto_frontier_RESp = []
+    pareto_frontier_GFA = []
+    pareto_frontier_PDG = []
+    pareto_frontier_d = []
+    pareto_frontier_w = []
+    pareto_frontier_r = []
+    pareto_frontier_RES = []
+    pareto_frontier_COM = []
+    pareto_frontier_OFF = []
+    fitness_values = book.sort_values(['RES%', 'GFA-m2', 'PDG-Kwh'],
+                                      ascending=[False, False, True])
+    # fitness_values = pd.DataFrame({'RES%':[1,1,1], 'GFA-m2': [0,1,-1], 'PDG-Kwh':[1,1,-1]})
+    print (fitness_values)
+    print (book)
+    a = fitness_values['RES%'].idxmax(axis = 0)
+    b = a
+    print (book['RES'][b])
+    print (fitness_values['RES'][b])
+    pareto_frontier_RESp.append(book['RES%'][b])
+    pareto_frontier_GFA.append(book['GFA-m2'][b])
+    pareto_frontier_PDG.append(book['PDG-Kwh'][b])
+    pareto_frontier_d.append(book['d'][b])
+    pareto_frontier_w.append(book['w'][b])
+    pareto_frontier_r.append(book['r'][b])
+    pareto_frontier_RES.append(book['RES'][b])
+    pareto_frontier_COM.append(book['COM'][b])
+    pareto_frontier_OFF.append(book['OFF'][b])
+    print (len(fitness_values))
+    for row in xrange(0, len(fitness_values)):
+        dominated = False
+        for j in xrange(0, len(pareto_frontier_RESp)):
+            if (pareto_frontier_RESp[j] >= fitness_values['RES%'][row]):  # maximizing RES%
+                if (pareto_frontier_GFA[j] >= fitness_values['GFA-m2'][row]):  # maximizing GFA-m2
+                    if (pareto_frontier_PDG[j] <= fitness_values['PDG-Kwh'][row]):  # minimizing PDG-Kwh
+                        print (pareto_frontier_PDG[j], pareto_frontier_GFA[j], pareto_frontier_RESp[j])
+                        print (fitness_values['PDG-Kwh'][row], fitness_values['GFA-m2'][row],fitness_values['RES%'][row])
+                        dominated = True
+                        break
+        if not dominated:
+            pareto_frontier_RESp.append(fitness_values['RES%'][row])
+            pareto_frontier_GFA.append(fitness_values['GFA-m2'][row])
+            pareto_frontier_PDG.append(fitness_values['PDG-Kwh'][row])
+            pareto_frontier_d.append(fitness_values['d'][row])
+            pareto_frontier_w.append(fitness_values['w'][row])
+            pareto_frontier_r.append(fitness_values['r'][row])
+            pareto_frontier_RES.append(fitness_values['RES'][row])
+            pareto_frontier_COM.append(fitness_values['COM'][row])
+            pareto_frontier_OFF.append(fitness_values['OFF'][row])
+
+    # print (type(pareto_frontier_RES))
+    pareto_frontier = pd.DataFrame({'RES%': pareto_frontier_RESp, 'GFA-m2': pareto_frontier_GFA, 'PDG-Kwh': pareto_frontier_PDG,
+                                    'r': pareto_frontier_r, 'w': pareto_frontier_w, 'd': pareto_frontier_d,
+                                    'RES': pareto_frontier_RES, 'COM': pareto_frontier_COM, 'OFF': pareto_frontier_OFF})
+    # pareto_frontier = pd.DataFrame(
+    #     {'RES%': pareto_frontier_RESp, 'GFA': pareto_frontier_GFA, 'PDG': pareto_frontier_PDG})
+    pareto_frontier.to_csv('C:\Users\Bhargava\Downloads\\abc.csv')
+    print (pareto_frontier)
+    return pareto_frontier_RES
+
+
+
+if __name__ == '__main__':
+
+    pop = []
+    book = pd.read_excel(open('C:\Users\Bhargava\Downloads\Solar_DDD_5.xlsx', 'rb'))
+    # del book['d']
+    # del book['w']
+    # del book['r']
+    # del book['RES']
+    # del book['COM']
+    # del book['OFF']
+    # del book['PVE-Kwh']
+    # del book['PDG-Kwh']
+    # del book['Unnamed: 6']
+    # RES = book['RES%']
+    # GFA = book['GFA-m2']
+    # PDG-Kwh = book['PDG-Kwh']
+    # print (RES, GFA, PDG-Kwh)
+
+    identify_pareto_observations(book)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
